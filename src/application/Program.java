7f69a4cc59790785c04a44af0b238c12a1b72bfb
gabriel marques <gabrielmarksf@icloud.com>
2024-02-24 13:20:23 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
-public class Program{
+import boardgame.Position;
+
+public class Program {
   public static void main(String[] args) {
 
-    System.out.println("oi");
+    Position position = new Position(3, 5);
+    System.out.println(position);
 
   }
 }
